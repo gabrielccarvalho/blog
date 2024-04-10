214da1921e023e02d19645ee977564de57108d09
Gabriel <caampos22@gmail.com>
2024-04-10 09:49:21 -0300

    Feat ✨: Improve UI on landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,28 @@
 import { MaxWidthWrapper } from '@/components/max-width-wrapper'
+import { Button } from '@/components/ui/button'
+import { ArrowRightIcon } from '@radix-ui/react-icons'
 
 export default function Home() {
 	return (
 		<MaxWidthWrapper className='flex flex-col md:min-h-[calc(100vh-8rem)] h-[calc(100vh-11rem)] justify-center p-6 py-16 gap-8'>
-			<h1 className='text-4xl font-extrabold font-display'>Gabriel Campos</h1>
+			<h1 className='text-4xl font-extrabold font-display px-4'>
+				Gabriel Campos
+			</h1>
 			<div className='flex flex-col gap-1'>
-				<span>Software Engineer</span>
-				<span className='text-gray-500'>
+				<span className='px-4'>Software Engineer</span>
+				<span className='text-gray-500 px-4'>
 					Passionate about javascript and open-source.
 				</span>
-				<div className='flex items-center self-start rounded-lg mt-4 group'>
-					<span>
-						Press <kbd className='bg-gray-600 px-1.5 py-0.5 rounded-sm'>⌘</kbd>{' '}
-						<kbd className='bg-gray-600 px-1.5 py-0.5 rounded-sm'>k</kbd> to
-						start
-					</span>
+				<div className='mt-4'>
+					<Button variant='ghost' className='hover:bg-white/10'>
+						<span>
+							Press{' '}
+							<kbd className='bg-gray-600 px-1.5 py-0.5 rounded-sm'>⌘</kbd>{' '}
+							<kbd className='bg-gray-600 px-1.5 py-0.5 rounded-sm'>k</kbd> to
+							start
+						</span>
+						<ArrowRightIcon className='size-4 ml-2' />
+					</Button>
 				</div>
 			</div>
 		</MaxWidthWrapper>
